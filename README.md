6792c83ee6214f66078cdeb2c0f0aed78214f447
Naser Rafinia <Naser.r@gmail.com>
2021-04-19 17:31:17 +0430

    Transfer from the subscene project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Emby.Subtitle.Sub2fm
+# Emby.Subtitle.Subscene
+Subscene subtitle provider for Emby
+
+Currently support languages:
+
+* farsi_persian
+* arabic
+* english
+* burmese
+* danish
+* dutch
+* hebrew
+* indonesian
+* korean
+* malay
+* spanish
+* vietnamese
+* turkish
+* bengali
+* bulgarian
+* croatian
+* finnish
+* french
+* german
+* greek
+* hungarian
+* italian
+* kurdish
+* macedonian
+* malayalam
+* norwegian
+* portuguese
+* russian
+* serbian
+* sinhala
+* slovenian
+* swedish
+* thai
+* urdu
+* Brazilian Portuguese
---
+++ src/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Emby.Subtitle.Subscene.Providers;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Providers;
+
+namespace Test
+{
+    class Program
+    {
+        private const string Domain = "https://subscene.com";
+        private const string SearchApi = "{0}/subtitles/searchbytitle?query={1}&l=";
+
+        static async Task Main(string[] args)
+        {
+            var txt = "The Old Guard";
+            var lang = "per";
+            var searchResult = await new SubsceneSubtitleProvider(null, null, null, null, null)
+                .Search(txt, 2020, lang, VideoContentType.Movie, "547016", 0, 0);
+
+            Console.WriteLine("Result:");
+            foreach (var item in searchResult)
+            {
+                Console.WriteLine($"{item.Name} - {item.Id}");
+            }
+
+            /*await new SubsceneSubtitleProvider(null, null, null, null).GetSubtitles(
+                "__subtitles__bright__farsi_persian__1922088___per", CancellationToken.None);*/
+
+
+            //var text = File.ReadAllText(file, Encoding.GetEncoding(codePage));
+
+            Console.ReadKey();
+        }
+    }
+}
